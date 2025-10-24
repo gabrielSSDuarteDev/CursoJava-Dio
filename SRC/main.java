b8a1ac4eeb3e8529e3311a6baa56b2fe952ba923
Gabriel Duarte <gabrielssduarte.dev@gmail.com>
2025-10-23 22:40:50 -0300

    test: First "Hello World" em java

---
+++ SRC/main.java
@@ -0,0 +1,12 @@
+
+
+
+    public class main {
+        public static void main(String[] args) {
+            String mensagem = "Olá";
+            System.out.println(mensagem + args[0]);
+            System.out.println(mensagem + args[1]);
+            System.out.println(mensagem + args[2]);
+        }
+
+    }
